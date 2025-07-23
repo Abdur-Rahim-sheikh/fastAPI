cb36bd4c12ef69b8682f0b2c8b4f726c326f72d5
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-23 20:26:11 +0600

    query and path parameter
    fastapi understands path parameter and query smartly

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from enum import Enum
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-@app.get("/files/{file_path:path}/other")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
