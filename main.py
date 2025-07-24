3b6df9d86a67ab6465ede30577c49d1d6141acac
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-24 17:09:58 +0600

    after validator

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+import random
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from pydantic import AfterValidator
 
 app = FastAPI()
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
 
 
 @app.get("/items/")
 async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-        ),
-    ] = None,
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
