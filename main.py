77268236daeef51f582f0a2064236a2c86e311f9
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-24 17:50:16 +0600

    body with multiple parameters  and singular body part

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from typing import Annotated, Literal
+from typing import Annotated
 
-from fastapi import FastAPI, Query
-from pydantic import BaseModel, Field
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class FilterParams(BaseModel):
-    limit: int = Field(100, gt=0, le=100)
-    offset: int = Field(0, ge=0)
-    order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list[str] = []
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
-@app.get("/items/")
-async def read_items(filter_query: Annotated[FilterParams, Query()]):
-    return filter_query
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
