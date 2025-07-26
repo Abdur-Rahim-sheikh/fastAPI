13c3082a219e0081bdd5e5ce16deedc4be809e1e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-26 19:55:44 +0600

    upload file

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 from typing import Annotated
 
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    return results
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
