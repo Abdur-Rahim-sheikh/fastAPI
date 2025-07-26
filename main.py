2d326b2a230ad2238ca00feb94d0c5a5a4cf4cf0
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-26 14:48:04 +0600

    passing header params as `Query`, `Params` and `Cookie`

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 from typing import Annotated
 
-from fastapi import Body, FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    return results
+@app.get("/items/")
+async def read_items(user_agent: Annotated[str | None, Header()] = None):
+    return {"User-Agent": user_agent}
