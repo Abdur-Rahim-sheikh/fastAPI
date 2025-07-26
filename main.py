38cc8d62d3dd15fae9181caebe1c74ea765ea887
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-27 01:14:43 +0600

    dependency injection

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from typing import Annotated
+
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.get("/items")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+class User(BaseModel):
+    username: str
+    email: str
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded",
+        email="abir@gmail.com",
+        full_name="Abir",
+    )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
