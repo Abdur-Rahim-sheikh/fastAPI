2551bf60b3ed0b3886ec6e05d4527cd21b57c809
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-26 14:58:13 +0600

    multiple header as model with restricting extra header

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class CommonHeaders(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    host: str
+    save_data: bool
+    if_modified_since: str | None = None
+    traceparent: str | None = None
+    x_tag: list[str] = []
+
+
 @app.get("/items/")
-async def read_items(user_agent: Annotated[str | None, Header()] = None):
-    return {"User-Agent": user_agent}
+async def read_items(headers: Annotated[CommonHeaders, Header()]):
+    return headers
