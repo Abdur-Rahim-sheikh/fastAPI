3735454fdac7d858060ebb70db4e924e4087b40f
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-26 21:19:50 +0600

    copying BaseModel of pydantic is a piece of cake

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-from typing import Annotated
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
+    name: str | None = None
     description: str | None = None
-    price: float
-    tax: float | None = None
+    price: float | None = None
+    tax: float = 10.5
+    tags: list[str] = []
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
 
 
-@app.put("/items/{item_id}")
-async def update_item(
-    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    return results
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.model_dump(exclude_unset=True)
+    updated_item = stored_item_model.model_copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
