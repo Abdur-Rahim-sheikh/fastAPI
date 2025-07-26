b8a2930d49fa8e8c0078c46b68dc8417e022d1cd
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-26 23:47:42 +0600

    class dependency

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from fastapi import Depends, FastAPI
 app = FastAPI()
 
 
-async def common_parameters(q: str = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+class CommonQueryParams:
+    def __init__(self, q: str = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 
 @app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_items(commons: Annotated[CommonQueryParams, Depends()]):
     return commons
 
 
 @app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_users(commons: Annotated[CommonQueryParams, Depends()]):
     return commons
