d1d76df174b6d6c347472c4fde7e9cf5c6d94f6a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-07-29 08:42:57 +0600

    story model defined

---
+++ backend/models/story.py
@@ -0,0 +1,33 @@
+from sqlalchemy import JSON, Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from db.database import Base
+
+
+class Story:
+    __tablename__ = "stories"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, index=True)
+    session_id = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+    nodes = relationship("StoryNode", back_populates="story")
+
+
+class StoryNode(Base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+    options = Column(JSON, default={})
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+    story = relationship(Story, back_populates="nodes")
